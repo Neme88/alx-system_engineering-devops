abb2e3d777d90e793c122294b01832020147efb7
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2023-06-23 18:27:44 +0200

    added 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a Puppet file in /tmp
+
+file { '/tmp/school':
+        ensure  => present,
+        content => 'I love Puppet',
+        mode    => '0744', # Set file permissions to 0774
+        owner   => 'www.data',
+        group   => 'www.data',
+}
