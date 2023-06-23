db157d0480b320145ef76b4ea79b727313e33b0f
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2023-06-23 18:53:41 +0200

    added fixed bug in 0-create_a_file.pp file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,6 +4,6 @@ file { '/tmp/school':
         ensure  => present,
         content => 'I love Puppet',
         mode    => '0744', # Set file permissions to 0774
-        owner   => 'www.data',
-        group   => 'www.data',
+        owner   => 'www-data',
+        group   => 'www-data',
 }
